a665f45cf0647b43548401bf0b5bdbe1451cc29d
WicklowWolf <92536730+Destination2Unknown@users.noreply.github.com>
2022-01-07 16:22:42 +0000

    Create step1.py

---
+++ step1.py
@@ -0,0 +1,25 @@
+from tkinter import filedialog as fd
+import os
+
+class openfile(object):
+     def __init__(
+        self,
+        filepath=os.getcwd(), 
+        filename=os.getcwd() + '\PRC.csv',
+    ):
+        self.filepath=filepath
+        self.filename=filename
+                
+     def __call__(self):
+            filetypes = (
+                ('CSV files', '*.csv'),
+                ('All files', '*.*')
+            )
+            oldfilename=self.filename
+            self.filename= fd.askopenfilename(
+                title='Open a file',
+                initialdir=self.filepath,
+                filetypes=filetypes)
+            if self.filename=="":
+                self.filename =oldfilename
+            self.filepath=os.path.dirname(self.filename)
